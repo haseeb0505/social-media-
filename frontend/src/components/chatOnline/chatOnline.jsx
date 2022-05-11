0aa7ca26de92dcdfc156db486a788e1b20aa56ed
Haseeb zahid <haseebzahid6@gmail.com>
2022-05-11 22:37:02 +0500

    error in online friends

--- frontend/src/components/chatOnline/chatOnline.jsx
+++ frontend/src/components/chatOnline/chatOnline.jsx
@@ -1,17 +1,48 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 import "./chatOnline.css";
 
-export default function chatOnline() {
+export default function ChatOnline({ onlineUsers, currentId, setCurrentChat }) {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const [friends, setFriends] = React.useState([]);
+  const [onlineFriends, setOnlineFriends] = React.useState([]);
+  useEffect(() => {
+    const getFriends = async () => {
+      try {
+        const res = await axios.get("/users/following/" + currentId);
+        setFriends(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getFriends();
+  }, [currentId]);
+
+  useEffect(() => {
+    setOnlineFriends(friends.filter((f) => onlineUsers.includes(f._id)));
+  }, [onlineUsers, friends]);
+
   return (
     <div>
-      <div className="chatOnlineFriend">
-        <div className="chatOnlineImgContainer">
-          <img className="chatOnlineImg" src={PF + "person/7.jpeg"} alt="" />
-          <div className="chatOnlineBadge"></div>
-        </div>
-        <span className="chatOnlineUsername">john Doe</span>
-      </div>
+      {onlineFriends.map((o) => (
+        <>
+          <div className="chatOnlineFriend">
+            <div className="chatOnlineImgContainer">
+              <img
+                className="chatOnlineImg"
+                src={
+                  o?.profilePicture
+                    ? PF + o.profilePicture
+                    : PF + "person/noAvatar.png"
+                }
+                alt=""
+              />
+              <div className="chatOnlineBadge"></div>
+            </div>
+            <span className="chatOnlineUsername">{o?.username}</span>
+          </div>
+        </>
+      ))}
     </div>
   );
 }
--- frontend/src/pages/messenger/messenger.jsx
+++ frontend/src/pages/messenger/messenger.jsx
@@ -13,6 +13,7 @@ export default function Messenger() {
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
   const [arrivalMessage, setArrivalMessage] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState([]);
   const { user } = useContext(AuthContext);
   const scrollRef = useRef();
   const socket = useRef();
@@ -36,9 +37,12 @@ export default function Messenger() {
   useEffect(() => {
     socket.current.emit("addUser", user._id);
     socket.current.on("getUsers", (users) => {
-      console.log("hello", users);
+      setOnlineUsers(
+        user.following.filter((f) => users.some((u) => u.userId === f))
+      );
     });
   }, [user]);
+
   useEffect(() => {
     const getConversation = async () => {
       try {
@@ -138,7 +142,11 @@ export default function Messenger() {
         </div>
         <div className="chatOnline">
           <div className="chatOnlineWrapper">
-            <ChatOnline />
+            <ChatOnline
+              onlineUsers={onlineUsers}
+              currentId={user._id}
+              setCurrentChat={SetCurrentChat}
+            />
           </div>
         </div>
       </div>
